7794bd9d63e28354b23d38e24af30f0c6bf02028
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2024-08-23 12:11:49 -0300

    tentando entender o expo router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,11 +27,8 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="play" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
       </Stack>
-    </ThemeProvider>
   );
 }
