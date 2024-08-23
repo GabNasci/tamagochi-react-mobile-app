d0ed0797d8704832dbc50cfcb45cee9342815898
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2024-08-23 10:29:22 -0300

    implementando header e background

--- app/play.tsx
+++ app/play.tsx
@@ -1,34 +1,66 @@
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Font from 'expo-font';
+import { useFonts } from 'expo-font';
 
-const styles = StyleSheet.create({
-    safeAreaContainer: {
-        flex: 1,
-        backgroundColor: "white"
-    },
 
-    container: {
-        flex: 1
-    },
-    image: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-})
 
-const image = { uri: '../../../assets/images/background_play.png' }
 
 const play = () => {
+
+    const [fontsLoaded] = useFonts({
+        'supercell-font': require('@/assets/fonts/supercell-magic.ttf'),
+        // Adicione outras fontes aqui
+    });
+
+
+
     return (
         <SafeAreaView style={styles.safeAreaContainer}>
-            <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+            <ImageBackground source={require('@/assets/images/background_play.png')} resizeMode="cover" style={styles.image}>
                 <View style={styles.container}>
-                    <Text>Lal</Text>
+                    <View style={styles.headerContainer}>
+                        <Text style={styles.text}>Pato</Text>
+                    </View>
                 </View>
             </ImageBackground>
 
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    safeAreaContainer: {
+        flex: 1,
+        backgroundColor: "grey"
+    },
+
+    container: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: 'center',
+        paddingHorizontal: 32
+
+    },
+    headerContainer: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: 'center',
+        paddingVertical: 130
+
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    text: {
+        fontFamily: 'supercell-font',
+        color: "white",
+        fontSize: 40,
+        textShadowColor: 'black',
+        textShadowOffset: { width: 2, height: 2 },
+        textShadowRadius: 2,
+    }
+})
 
 export default play;
