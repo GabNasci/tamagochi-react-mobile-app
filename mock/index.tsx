3c50c6daa97fdb7a213831d9da79e73335e0db81
Jackson Martin <jackson.martin@grupointegrado.br>
2024-09-06 19:45:49 -0300

    adicionando mock e backgrondHungry

--- mock/index.tsx
+++ mock/index.tsx
@@ -36,3 +36,42 @@ export const duckGifs = [
         swim: require('@/assets/images/pato-marelo/yellow-swim-animation.png')
     }
 ]
+
+export const foodImage = [
+    { name: 'Albacore', image: require('@/assets/images/food/Albacore.png') },
+    { name: 'Anchovy', image: require('@/assets/images/food/Anchovy.png') },
+    { name: 'Angler', image: require('@/assets/images/food/Angler.png') },
+    { name: 'Bream', image: require('@/assets/images/food/Bream.png') },
+    { name: 'Carp', image: require('@/assets/images/food/Carp.png') },
+    { name: 'Catfish', image: require('@/assets/images/food/Catfish.png') },
+    { name: 'Crimsonfish', image: require('@/assets/images/food/Crimsonfish.png') },
+    { name: 'Glacierfish', image: require('@/assets/images/food/Glacierfish.png') },
+    { name: 'Ice_Pip', image: require('@/assets/images/food/Ice_Pip.png') },
+    { name: 'Herring', image: require('@/assets/images/food/Herring.png') },
+    { name: 'Largemouth_Bass', image: require('@/assets/images/food/Largemouth_Bass.png') },
+    { name: 'Legend', image: require('@/assets/images/food/Legend.png') }, // corrigido para 'Legend'
+    { name: 'Midnight_Carp', image: require('@/assets/images/food/Midnight_Carp.png') },
+    { name: 'Midnight_Squid', image: require('@/assets/images/food/Midnight_Squid.png') },
+    { name: 'Mutant_Carp', image: require('@/assets/images/food/Mutant_Carp.png') },
+    { name: 'Octopus', image: require('@/assets/images/food/Octopus.png') },
+    { name: 'Perch', image: require('@/assets/images/food/Perch.png') },
+    { name: 'Pike', image: require('@/assets/images/food/Pike.png') },
+    { name: 'Pufferfish', image: require('@/assets/images/food/Pufferfish.png') },
+    { name: 'Rainbow_Trout', image: require('@/assets/images/food/Rainbow_Trout.png') },
+    { name: 'Red_Mullet', image: require('@/assets/images/food/Red_Mullet.png') },
+    { name: 'Red_Snapper', image: require('@/assets/images/food/Red_Snapper.png') },
+    { name: 'Salmon', image: require('@/assets/images/food/Salmon.png') },
+    { name: 'Sardine', image: require('@/assets/images/food/Sardine.png') },
+    { name: 'Sea_Cucumber', image: require('@/assets/images/food/Sea_Cucumber.png') },
+    { name: 'Slimejack', image: require('@/assets/images/food/Slimejack.png') },
+    { name: 'Smallmouth_Bass', image: require('@/assets/images/food/Smallmouth_Bass.png') },
+    { name: 'Spook_Fish', image: require('@/assets/images/food/Spook_Fish.png') },
+    { name: 'Stonefish', image: require('@/assets/images/food/Stonefish.png') },
+    { name: 'Sturgeon', image: require('@/assets/images/food/Sturgeon.png') },
+    { name: 'Sunfish', image: require('@/assets/images/food/Sunfish.png') },
+    { name: 'Tiger_Trout', image: require('@/assets/images/food/Tiger_Trout.png') },
+    { name: 'Tuna', image: require('@/assets/images/food/Tuna.png') },
+    { name: 'Void_Salmon', image: require('@/assets/images/food/Void_Salmon.png') },
+    { name: 'Walleye', image: require('@/assets/images/food/Walleye.png') },
+  ];
+  
